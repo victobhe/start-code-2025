0e2459dc99a98cb77c005c3b2d8817cdbb707226
simenen123 <Simenlog@outlook.com>
2025-10-17 16:45:39 +0200

    Feat: fiksa server og api call

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # start-code-2025
 Our solution to the Case 2 at the Hackathon 17. oktober - 19. oktober 2025
+
+
+## Server
+for å fikse server, så må man kjøre disse kommandoene
+*
+cd backend
+npm init -y
+npm i express
+*
+Man kjører deretter serveren med
+*npm start*
---
+++ backend/api.js
@@ -0,0 +1,16 @@
+
+async function getData() {
+  const url = "http://localhost:3000/api/products";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+getData();
